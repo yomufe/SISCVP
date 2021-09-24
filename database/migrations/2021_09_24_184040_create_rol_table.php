24ffc1fdaaef0d2288585b97d0e3a783386ce359
anthonyrm98 <amanuelrm98@live.com>
2021-09-24 12:53:31 -0600

    Agregando atributos a tabla rol

--- database/migrations/2021_09_24_184040_create_rol_table.php
+++ database/migrations/2021_09_24_184040_create_rol_table.php
@@ -15,6 +15,7 @@ class CreateRolTable extends Migration
     {
         Schema::create('rol', function (Blueprint $table) {
             $table->id();
+            $table->string('nombre');
             $table->timestamps();
         });
     }
